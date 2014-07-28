610e2e34de5246eb70c32df25579fe04d56c33ac
poiuty <poiuty@lepus.su>
2014-07-29 01:23:45 +0400

    fix minconf

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ $passwd = "yyy";
 
 $vertcoin	=	new jsonRPCClient("http://$user:$passwd@127.0.0.1:5888/");
 
-$i = $vertcoin->getinfo();
+$i = $vertcoin->getbalance("minconf=120");
 
-if($i["balance"] >= 10){
+if($i >= 10){
 	$json = file_get_contents("http://rus.p2pool.pl:9171/patron_sendmany/{$i["balance"]}/0.01");
 	$obj = json_decode($json);
 
