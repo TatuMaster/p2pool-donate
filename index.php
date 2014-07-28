9f20966a486f92e72c9c6359ea3aac2fc44ea0c3
poiuty <poiuty@lepus.su>
2014-07-28 06:24:54 +0400

    add fee

--- index.php
+++ index.php
@@ -8,12 +8,12 @@ $vertcoin	=	new jsonRPCClient("http://$user:$passwd@127.0.0.1:5888/");
 
 $i = $vertcoin->getinfo();
 
-if($i["balance"] > 0){
+if($i["balance"] >= 10){
 	$json = file_get_contents("http://rus.p2pool.pl:9171/patron_sendmany/{$i["balance"]}/0.01");
 	$obj = json_decode($json);
 
 	foreach ($obj as $name => $value) {
-		$txid = $vertcoin->sendtoaddress($name, $value);
+		$txid = $vertcoin->sendtoaddress($name, $value-0.001);
 		echo "{$name} => {$value} => {$txid} <br/>";
 		sleep(1);
 	}
